1dbb382baf500214f525fa6fa9fad09d4c78edd3
Pedro <pedro.mendes@opendatacenter.com.br>
2025-05-13 18:56:13 -0300

    ✨ Continuing the studies

---
+++ basics/constants.go
@@ -0,0 +1,19 @@
+package main
+
+const (
+	pi       = 3.14
+	GRSAVITY = 9.81
+)
+
+func main() {
+	const days int = 100
+
+	const (
+		monday        = 1
+		tuesday       = 2
+		wednesday     = 3
+		thursday  int = 4
+	)
+
+	name := "John Doe"
+}
